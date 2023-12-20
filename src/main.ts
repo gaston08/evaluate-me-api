7a5988924a194100392108f260fbd8cfa9723a9c
gaston pedraza <gaston08pedraza@gmail.com>
2023-12-20 16:19:31 -0300

    add morgan

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import morgan from 'morgan';
 import { doAuth } from 'auth';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
