b6d776e3fc4e8cba941f98399630de7abfe655e7
gaston pedraza <gaston08pedraza@gmail.com>
2023-12-20 16:27:14 -0300

    test simple route

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import morgan from 'morgan';
 import { doAuth } from 'auth';
 
+// controllers
+import * as indexController from './controllers/index';
+
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
@@ -9,9 +12,8 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
+// primary app routes
+app.get('/', indexController.index);
 
 app.post('/auth', (req, res) => {
   res.send(doAuth());
