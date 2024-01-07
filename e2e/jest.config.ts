ca020494326bcee2872dffa179dcd45405efa1c6
gaston pedraza <gaston08pedraza@gmail.com>
2024-01-07 16:40:44 -0300

    remove unnecessary files

--- e2e/jest.config.ts
+++ e2e/jest.config.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
 export default {
   displayName: 'e2e',
-  preset: '../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
   setupFiles: ['<rootDir>/src/support/test-setup.ts'],
